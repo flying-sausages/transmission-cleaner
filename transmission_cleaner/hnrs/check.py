71308c05107402293947ead30c4dc0d5a6f3e389
flying_sausages <23618693+flying-sausages@users.noreply.github.com>
2026-02-21 02:22:53 +0100

    fix tvcuk domain

--- transmission_cleaner/hnrs/check.py
+++ transmission_cleaner/hnrs/check.py
@@ -8,7 +8,7 @@ hnr_map: dict[str, Callable] = {
     "landof.tv": hnrs.btn.is_outside_hnr,
     "tracker.beyond-hd.me": hnrs.bhd.is_outside_hnr,
     "please.passthepopcorn.me": hnrs.ptp.is_outside_hnr,
-    "tvchaos.uk": hnrs.tvcuk.is_outside_hnr,
+    "tvchaosuk.com": hnrs.tvcuk.is_outside_hnr,
 }
 
 
