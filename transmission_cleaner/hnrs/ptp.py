e44e652c0e770ac2a18cf7d0e5d2659bbbfefd79
flying_sausages <23618693+flying-sausages@users.noreply.github.com>
2026-02-21 02:49:02 +0100

    fix transmission api usage for ptp

--- transmission_cleaner/hnrs/ptp.py
+++ transmission_cleaner/hnrs/ptp.py
@@ -12,6 +12,6 @@ def is_outside_hnr(torrent: Torrent) -> bool:
     PTP rules:
         Once you have downloaded at least 95% of a torrent, you must seed it for at least 48 hours within a 2 week period or until you have seeded at least one full copy (1:1).
     """
-    if torrent.ratio >= 1 or torrent.percent_done < 0.95:
+    if torrent.ratio >= 1 or torrent.percent_complete < 0.95:
         return True
     return torrent.seconds_seeding >= 48 * 3600
